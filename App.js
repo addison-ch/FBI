71d06a06a2bf579811b3ffcda87f1c99dd70d1cc
RishieNagarajah <57832213+RishieNagarajah@users.noreply.github.com>
2022-05-07 11:00:53 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+
+const divStyle = {
+  margin: '0px',
+  border: '3px'
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <div style={divStyle}> 
+        <p>
+          FBI
+          
+        </p>
+        </div>
+
+        <div className ="App-Words">
+        Food 'Bout Ingredients
+        </div>
+
+
+
+
+      </header>
+
+      <div className="Buttons">
+        <button>
+          Upload Photo
+        </button>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
