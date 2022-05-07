990b3497ea76f13130b95e651d0cdaa28e0add95
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-05-07 03:30:24 -0400

    added search to keywords

--- api/app.py
+++ api/app.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from serpapi import GoogleSearch
+import json, requests
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/api")
 def api():
-    return {"msg" : "hi rishie 🤰"}
+    params = {
+    "q": "What is the use of sodium chloride in food?",
+    "hl": "en",
+    "gl": "us",
+    "api_key": "1c1af191bef69e75c008dd69a150a7e2a688994a3ce696cdb784f52aa0d6ef0e"
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    answer_box = results["answer_box"]
+    
+    print(answer_box['snippet'])
+    print('hi')
+    return {"msg" : [{"ingredient": "Sodium Chloride", "fact": answer_box['snippet']}] }
 
 
 if __name__ == "__main__":
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
 import React, {useState, useEffect} from 'react';
-
+import './App.css';
 
 
 export default function App () {
   const [data, setData] = useState("");
-
+  const [show, setShow] = useState(false);
   useEffect(() => {
     fetch("http://localhost:5000/api").then((response) => {
       return response.json();
     }).then((response) => {
-      setData(response.msg);
-      console.log(response);
+      setData(response.msg[0])
+      console.log(response.msg);
     })}, [])
   
-
-  return (
+  // return array.map(function(each){
+  //   return(<h1>hello {each.name}</h1>)
+  // })
+  return(
     <>
-    <div>{data}</div>
-    <p></p></>
+      {/* {data.map(item => <div className="content">
+        <button className="collapsible" onClick={() =>setShow(!show)}>{item["ingredient"]}</button>
+        {
+          show?<div>{item["fact"]}</div>:null
+        }
+      </div>)}  */}
+      <div className="content">
+        <button className="collapsible" onClick={() =>setShow(!show)}>{data["ingredient"]}</button>
+        {
+          show?<div>{data["fact"]}</div>:null
+        }
+      </div>
+      <h1>hi</h1>
+    </>
   )
 }
 
