bfe8fec7848f16201b6d69834acee056eb392b8f
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-05-07 16:44:09 -0400

    google search

--- api/app.py
+++ api/app.py
@@ -9,19 +9,19 @@ CORS(app)
 
 @app.route("/api")
 def api():
-    params = {
-    "q": "What is the use of sodium chloride in food?",
-    "hl": "en",
-    "gl": "us",
-    "api_key": "1c1af191bef69e75c008dd69a150a7e2a688994a3ce696cdb784f52aa0d6ef0e"
-    }
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    answer_box = results["answer_box"]
-    
-    print(answer_box['snippet'])
-    print('hi')
-    return {"msg" : [{"ingredient": "Sodius Chloride", "fact": answer_box['snippet']}] }
+    # params = {
+    # "q": "What is the use of sodium chloride in food?",
+    # "hl": "en",
+    # "gl": "us",
+    # "api_key": "1c1af191bef69e75c008dd69a150a7e2a688994a3ce696cdb784f52aa0d6ef0e"
+    # }
+    # search = GoogleSearch(params)
+    # results = search.get_dict()
+    # answer_box = results["answer_box"]
+    # "fact": answer_box['snippet']
+    # print(answer_box['snippet'])
+    # print('hi')
+    return {"msg" : [{"ingredient": "Sodius Chloride", "fact": "It is healthy"}, {"ingredient": "Sodium Chloride"}] }
 
 
 if __name__ == "__main__":
--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,39 @@ export default function App () {
     fetch("http://localhost:5000/api").then((response) => {
       return response.json();
     }).then((response) => {
-      setData(response.msg[0])
-      console.log(response.msg);
+      setData(response.msg)
+      // console.log(response.msg);
     })}, [])
-  
+  var initialValue = [
+    "Star Wars", "Return of the Jedi", "Empire Strikes Back",
+  ]
+  console.log(data[0])
+  for(let i = 0; i<=1; i++){
+      // console.log(data[0])
+
+      //console.log(data['msg'][i])
+      // initialValue = initialValue.push('hi')
+      initialValue.push("Kiwi"+i);
+      // initialValue.push(data['msg'][i]['ingredient'])
+
+      //console.log(data['msg'].length)
+  //   // console.log(data.length)
+  }
+  // items = items.push(item)
+  // this.setState({items})
+  // console.log(data)
+  const [names, setNames] = useState(initialValue);
+  const steps = [];
+  for (let i = 1; i <= 2; i++) {
+    steps.push(
+      <div key={i} className="content">
+        <button className="collapsible" onClick={({i}) =>setShow(!show)}>{data["ingredient"]}</button>
+        {
+          show?<div>{data["fact"]}</div>:null
+        }
+      </div>
+    );
+  }
   // return array.map(function(each){
   //   return(<h1>hello {each.name}</h1>)
   // })
@@ -24,12 +53,12 @@ export default function App () {
           show?<div>{item["fact"]}</div>:null
         }
       </div>)}  */}
-      <div className="content">
-        <button className="collapsible" onClick={() =>setShow(!show)}>{data["ingredient"]}</button>
-        {
-          show?<div>{data["fact"]}</div>:null
-        }
+      <div>
+        {names.map((name) => {
+          return <div key={name}>{name}</div>;
+        })}
       </div>
+      <div className="progressBar">{ steps }</div>
       <h1>hi</h1>
     </>
   )
