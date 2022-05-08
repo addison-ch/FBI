8915bfba86306623035f7b6267345111e9e85553
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-05-08 01:39:27 -0400

    Update ingredients styling

--- client/src/App.js
+++ client/src/App.js
@@ -8,28 +8,43 @@ export default function App ({ingredients, facts}) {
   const [show, setShow] = useState(false);
   const ingr = ingredients;
   const fax = facts;
-
-
-
-
+  const fullList = []
+  for(let i = 0; i<ingr.length; i++){
+    fullList.push({
+      key:   ingredients[i],
+      value: fax[i]
+    });
+  }
+  
+  // for(let i = 0; i<ingr.length; i++){
+  //   fullList.push({ingredients[i]: fax[i]})
+  // }
+  // console.log(Object.keys(fullList[0]));
+  // console.log(fullList);
   return(
     <>
       <div>
-        {ingr.map((name) => {
+        {fullList.map((name) => {
           return (
-          <div key={name} className="content">
-                 {name}
-          </div>)
+            <>
+              <h3 key={name[Object.keys(name)[0]]} className="content">
+                    {name[Object.keys(name)[0]].charAt(0).toUpperCase() + name[Object.keys(name)[0]].slice(1)}  
+              </h3>
+              <div>
+                  {name[Object.keys(name)[1]]}
+              </div>
+            </>
+          )
         })}
       </div>
-      <div> 
+      {/* <div> 
       {fax.map((name) => {
           return (
           <div key={name} className="content">
                  {name}
           </div>)
         })}
-    </div>
+      </div> */}
     </>
   )
 }
