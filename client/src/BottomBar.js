227eb193e6ed276ebb8ce5c6dcd0b6e09efcc6f1
RishieNagarajah <57832213+RishieNagarajah@users.noreply.github.com>
2022-05-08 02:12:49 -0400

    added bottom bar

--- client/src/BottomBar.js
+++ client/src/BottomBar.js
@@ -1,7 +1,35 @@
 import React from 'react'
-
+import './BottomBar.css'
 export default  function BottomBar() {
   return (
-    <div>BottomBar</div>
+    <>
+    <div className="Bottom">
+
+      BottomBar
+
+      <div className="Copyright">
+        <p>
+        © Copyright Addougie 2022
+        </p>
+      </div>
+
+
+      <div className="Contact">
+        <p>
+          Contact Us
+        </p>
+      </div>
+
+
+      </div>
+
+
+
+
+
+
+
+    </>
+
   )
 }
